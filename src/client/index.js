bd888e62f5259932d5f6b4c38f5090152581000a
vwang4536 <vwang4536@gmail.com>
2019-01-14 21:50:08 -0800

    started building out react components, wait for debug tomorrow

--- src/client/index.js
+++ src/client/index.js
@@ -5,34 +5,49 @@ import Login from './components/routes/Login.jsx';
 import Lists from './components/routes/Lists.jsx';
 import history from './components/routes/history.jsx';
 
-class App extends React.Component { 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayLists: [],
+      displayListItems: []
+    }
+  }
   render() {
     return (
       <div className="main">
       hello
       </div>
-      <Router history={history}>
-        <Switch>
-          <Route exact path="/"
-            render={(props) =>
-              <Login {...props}
-              />
-            }
-          />
-          <Route path="/Lists"
-            render={(props) =>
-              <Lists {...props}
-              />
-            }
-          />
-          <Route path="/Groups"
-            render={(props) => 
-              <Groups {...props}
-              />
-            }
-          />
-        </Switch>
-      </Router>
+      // <Router history={history}>
+      //   <Switch>
+      //     <Route exact path="/"
+      //       render={(props) =>
+      //         <Login {...props}
+      //         />
+      //       }
+      //     />
+      //     <Route path="/Lists"
+      //       render={(props) =>
+      //         <Lists {...props}
+      //            parentState={this.state}
+      //         />
+      //       }
+      //     />
+      //     <Route path="/List"
+      //       render={(props) =>
+      //         <List {...props}
+      //            parentState={this.state} 
+      //       }
+      //     />
+      //     <Route path="/Groups"
+      //       render={(props) => 
+      //         <Groups {...props}
+      //            parentState={this.state}
+      //         />
+      //       }
+      //     />
+      //   </Switch>
+      // </Router>
     )
   }
 }
