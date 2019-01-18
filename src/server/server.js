a0613e9b94f3c14db710e8be606fae28a6ca17ad
Darryl Amour <darryl.amour@gmail.com>
2019-01-17 18:00:15 -0800

    Improved interaction with client in conjunction with oAuth. still trying to determine best approach

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
 
 // Retrieve Configuration Keys
@@ -15,9 +16,21 @@ const app = express();
 // Allow cross-origin requests betwen localhosts
 app.use(cors());
 
+// Parse Cookies
+app.use(cookieParser());
+
 // Initialize Passport
 app.use(passport.initialize());
 require('./passport');
+
+// CLEAR ANY SET COOKIES ASSUME FAILED oAuth
+app.get(
+  '/',
+  (req, res) => {
+    res.clearCookie('dontforget.token');
+    res.redirect(`${CLIENT_URL}`);
+  },
+);
 
 // Create a GraphQL endpoint
 app.use(
@@ -38,8 +51,14 @@ app.get(
   '/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/', session: false }),
   (req, res) => {
-    const { token } = req.user;
-    res.redirect(`${CLIENT_URL}?token=${token}`);
+    const { token, email, name } = req.user;
+
+    // set cookie dontforget.token
+    res.cookie('dontforget.token', token, { maxAge: 900000, httpOnly: true });
+
+    // Add user to database
+
+    res.redirect(`${CLIENT_URL}/Lists`);
   },
 );
 
